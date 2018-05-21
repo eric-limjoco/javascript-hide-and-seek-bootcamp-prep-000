6134b58405147d9174747004dfed410bdc4fbdc3
ericlimjoco@gmail.com Limjoco <ericlimjoco@gmail.com>
2018-05-21 11:11:40 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,31 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector)
+}
+
+function nestedTarget(){
+  return document.querySelector('#nested .target')
+}
+
+function increaseRankBy(n){
+  var lis = document.querySelectorAll('.ranked-list li')
+  
+  for(var i = 0; i < lis.length; i++){
+    lis[i].innerHTML = parseInt(lis[i].innerHTML) + n
+  }
+}
+
+function deepestChild(){
+  var node = document.getElementById('grand-node')
+  var current = node
+  var cont = true
+  
+  while(cont){
+    if(current.hasChildNodes()){
+      current = current.childNodes[0]
+    } else {
+      cont = false
+    }
+  }
+  return current
+
+}
